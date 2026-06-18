a6435003c1d0017eec3e2b564e039d51c9b28b2d
mzalazar1 <marcoszalazar86@gmail.com>
2026-06-18 00:10:29 -0300

    update sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -8,23 +8,5 @@ export default function sitemap() {
       changeFrequency: "weekly",
       priority: 1,
     },
-    {
-      url: `${baseUrl}/#beneficios`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#demo`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: `${baseUrl}/#planes`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.9,
-    },
   ];
 }
